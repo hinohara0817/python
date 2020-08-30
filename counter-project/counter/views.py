eb6d572ab8c2460f82ee5141a54945f16cbe1dd8
hinohara <hinoshin0817@gmail.com>
2020-08-30 11:45:16 +0900

    add templates

--- counter-project/counter/views.py
+++ counter-project/counter/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def homepage(reguest):
-  return HttpResponse("Hello")
+def homepage(request):
+  return render(request, "home.html")
